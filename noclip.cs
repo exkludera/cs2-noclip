3be21eb5c95c168ce88444efc58e4aaffe46abe1
70L <51145038+70L@users.noreply.github.com>
2024-04-18 22:29:24 +0200

    Update noclip.cs

--- noclip.cs
+++ noclip.cs
@@ -1,30 +1,21 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Utils;
 
-namespace Noclip;
-
-[MinimumApiVersion(80)]
 public class Noclip : BasePlugin
 {
     public override string ModuleName => "noclip";
-    public override string ModuleVersion => "1.0.1";
+    public override string ModuleVersion => "1.0.2";
     public override string ModuleAuthor => "exkludera";
     public override string ModuleDescription => "";
-
-    public override void Load(bool hotReload)
-    {
-
-    }
 
     [ConsoleCommand("css_noclip", "noclip command")]
     public void OnCmdNoclip(CCSPlayerController? player, CommandInfo command)
     {
-        if (blockCheck(player))
+        if (player == null || !player.PawnIsAlive || player.Team == CsTeam.Spectator || player.Team == CsTeam.None)
             return;
 
         if (player.PlayerPawn.Value.MoveType == MoveType_t.MOVETYPE_NOCLIP)
@@ -40,12 +31,5 @@ public class Noclip : BasePlugin
             Utilities.SetStateChanged(player.PlayerPawn.Value, "CBaseEntity", "m_MoveType");
         }
     }
-
-    private bool blockCheck(CCSPlayerController? player)
-    {
-        if (player == null || !player.PawnIsAlive || player.Team == CsTeam.Spectator || player.Team == CsTeam.None)
-            return true;
-        return false;
-    }
-
+    
 }
